e488f300bc029d90b35fa48fee1fe9d52bc97583
Gabriel Mori Baleta <moribaleta.work@gmail.com>
2021-07-30 15:13:05 +0800

    relayout

---
+++ ReNode/Classes/extensions/ReLayout.swift
@@ -0,0 +1,253 @@
+//
+//  ReLayout.swift
+//  SampleApp
+//
+//  Created by Gabriel Mori Baleta on 7/30/21.
+//
+
+import Foundation
+import AsyncDisplayKit
+import ReNode
+
+
+@resultBuilder
+public struct LayoutBuilder {
+    public static func buildExpression<Node: ASDisplayNode>(_ expression: Node?) -> ASLayoutElement? {
+        expression
+    }
+    
+    public static func buildExpression(_ expression: ASLayoutElement) -> [ASLayoutElement] {
+        [ expression ]
+    }
+    
+    public static func buildExpression(_ expression: ASLayoutElement?) -> [ASLayoutElement] {
+        if let expression = expression {
+            return [ expression ]
+        } else {
+            return []
+        }
+    }
+    
+    public static func buildExpression(_ expression: [ASLayoutElement]) -> [ASLayoutElement] {
+        expression
+    }
+    
+    public static func buildEither(first component: [ASLayoutElement]) -> [ASLayoutElement] {
+        component
+    }
+    
+    public static func buildEither(second component: [ASLayoutElement]) -> [ASLayoutElement] {
+        component
+    }
+    
+    public static func buildOptional(_ component: [ASLayoutElement]?) -> [ASLayoutElement] {
+        component ?? []
+    }
+    
+    public static func buildBlock(_ components: [ASLayoutElement]...) -> [ASLayoutElement] {
+        components.flatMap { $0 }
+    }
+    
+    public static func buildBlock(_ components: ASLayoutElement?...) -> [ASLayoutElement] {
+        components.compactMap { $0 }
+    }
+}
+
+extension ASWrapperLayoutSpec {
+    public convenience init(@LayoutBuilder _ children: () -> [ASLayoutElement]) {
+        self.init(layoutElements: children())
+    }
+}
+
+extension ASStackLayoutSpec {
+    
+    
+    
+    public func spacing(_ spacing: CGFloat = 10) -> Self {
+        self.spacing = spacing
+        return self
+    }
+    
+    public func justify(_ justify: ASStackLayoutJustifyContent = .start) -> Self {
+        self.justifyContent = justify
+        return self
+    }
+    
+    public func align(_ align: ASStackLayoutAlignItems = .stretch) -> Self {
+        self.alignItems = align
+        return self
+    }
+    
+    public func wrap(_ wrap: ASStackLayoutFlexWrap = .wrap) -> Self {
+        self.flexWrap = wrap
+        return self
+    }
+    
+    public func linespacing(_ linespacing: CGFloat = 10) -> Self {
+        self.lineSpacing = linespacing
+        return self
+    }
+    
+    public func children(@LayoutBuilder _ children: () -> [ASLayoutElement]) -> Self {
+        self.children = children()
+        return self
+    }
+    
+    
+    
+    
+    
+    public static func horizontal  (spacing: CGFloat, justify: ASStackLayoutJustifyContent = .start, align: ASStackLayoutAlignItems = .start, wrap: ASStackLayoutFlexWrap = .noWrap, lineSpacing: CGFloat = 0, @LayoutBuilder _ children: () -> [ASLayoutElement]) -> ASStackLayoutSpec {
+        tell(ASStackLayoutSpec(direction: .horizontal, spacing: spacing, justifyContent: justify, alignItems: align, children: children())) {
+            $0.flexWrap = wrap
+            $0.lineSpacing = lineSpacing
+        }
+    }
+    public static func vertical    (spacing: CGFloat, justify: ASStackLayoutJustifyContent = .start, align: ASStackLayoutAlignItems = .start, wrap: ASStackLayoutFlexWrap = .noWrap, lineSpacing: CGFloat = 0, @LayoutBuilder _ children: () -> [ASLayoutElement]) -> ASStackLayoutSpec {
+        tell(ASStackLayoutSpec(direction: .vertical, spacing: spacing, justifyContent: justify, alignItems: align, children: children())) {
+            $0.flexWrap = wrap
+            $0.lineSpacing = lineSpacing
+        }
+    }
+    
+    public static func horizontal  (spacing: CGFloat, justify: ASStackLayoutJustifyContent = .start, align: ASStackLayoutAlignItems = .start, wrap: ASStackLayoutFlexWrap = .noWrap, lineSpacing: CGFloat = 0, _ children: [ASLayoutElement]) -> ASStackLayoutSpec {
+        tell(ASStackLayoutSpec(direction: .horizontal, spacing: spacing, justifyContent: justify, alignItems: align, children: children)) {
+            $0.flexWrap = wrap
+            $0.lineSpacing = lineSpacing
+        }
+    }
+    public static func vertical    (spacing: CGFloat, justify: ASStackLayoutJustifyContent = .start, align: ASStackLayoutAlignItems = .start, wrap: ASStackLayoutFlexWrap = .noWrap, lineSpacing: CGFloat = 0, _ children: [ASLayoutElement]) -> ASStackLayoutSpec {
+        tell(ASStackLayoutSpec(direction: .vertical, spacing: spacing, justifyContent: justify, alignItems: align, children: children)) {
+            $0.flexWrap = wrap
+            $0.lineSpacing = lineSpacing
+        }
+    }
+}
+
+extension ASDisplayNode {
+    public convenience init(@LayoutBuilder _ children: @escaping () -> [ASLayoutElement]) {
+        self.init()
+        
+        automaticallyManagesSubnodes = true
+        
+        layoutSpecBlock = { node, size in
+            ASWrapperLayoutSpec(layoutElements: children())
+        }
+    }
+    
+    public func enabled(_ value: Bool) -> Self {
+        if value {
+            isUserInteractionEnabled = true
+            alpha = 1
+        } else {
+            isUserInteractionEnabled = false
+            alpha = 0.5
+        }
+        
+        return self
+    }
+}
+
+extension ASInsetLayoutSpec {
+    
+    public func insets(_ insets: UIEdgeInsets) -> Self{
+        self.insets = insets
+        return self
+    }
+    
+    public func child(_ child: ASLayoutElement) -> Self {
+        self.child = child
+        return self
+    }
+    
+}
+
+extension ASLayoutElement {
+    
+    public static func horizontalStack (@LayoutBuilder _ children: () -> [ASLayoutElement]) -> ASStackLayoutSpec {
+        return ASStackLayoutSpec.horizontal(spacing: 10, children)
+    }
+    
+    public static func verticalStack (@LayoutBuilder _ children: () -> [ASLayoutElement]) -> ASStackLayoutSpec {
+        return ASStackLayoutSpec.vertical(spacing: 10, children)
+    }
+    
+    public func inset(edges: UIEdgeInsets) -> ASLayoutSpec {
+        ASInsetLayoutSpec(insets: edges, child: self)
+    }
+    
+    public func centered(_ compressionAxes: ASCenterLayoutSpecSizingOptions, _ positionAxes: ASCenterLayoutSpecCenteringOptions) -> ASLayoutSpec {
+        ASCenterLayoutSpec(centeringOptions: positionAxes, sizingOptions: compressionAxes, child: self)
+    }
+    
+    public func flex(grow: CGFloat = 1, shrink: CGFloat = 0) -> Self {
+        style.flexGrow = grow
+        style.flexShrink = shrink
+        return self
+    }
+    
+    public func selfAlign(_ align: ASStackLayoutAlignSelf) -> Self {
+        style.alignSelf = align
+        return self
+    }
+    
+    public func frameProportional(minWidth: CGFloat? = nil, width: CGFloat? = nil, maxWidth: CGFloat? = nil,
+                                  minHeight: CGFloat? = nil, height: CGFloat? = nil, maxHeight: CGFloat? = nil) -> Self {
+        
+        if let value = minWidth {
+            style.minWidth = .init(unit: .fraction, value: value)
+        }
+        
+        if let value = width {
+            style.width = .init(unit: .fraction, value: value)
+        }
+        
+        if let value = maxWidth {
+            style.maxWidth = .init(unit: .fraction, value: value)
+        }
+        
+        if let value = minHeight {
+            style.minHeight = .init(unit: .fraction, value: value)
+        }
+        
+        if let value = height {
+            style.height = .init(unit: .fraction, value: value)
+        }
+        
+        if let value = maxHeight {
+            style.maxHeight = .init(unit: .fraction, value: value)
+        }
+        
+        return self
+    }
+    
+    public func frame(minWidth: CGFloat? = nil, width: CGFloat? = nil, maxWidth: CGFloat? = nil,
+                      minHeight: CGFloat? = nil, height: CGFloat? = nil, maxHeight: CGFloat? = nil) -> Self {
+        
+        if let value = minWidth {
+            style.minWidth = .init(unit: .points, value: value)
+        }
+        
+        if let value = width {
+            style.width = .init(unit: .points, value: value)
+        }
+        
+        if let value = maxWidth {
+            style.maxWidth = .init(unit: .points, value: value)
+        }
+        
+        if let value = minHeight {
+            style.minHeight = .init(unit: .points, value: value)
+        }
+        
+        if let value = height {
+            style.height = .init(unit: .points, value: value)
+        }
+        
+        if let value = maxHeight {
+            style.maxHeight = .init(unit: .points, value: value)
+        }
+        
+        return self
+    }
+}
